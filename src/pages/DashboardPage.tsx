20dee58042f31f32ea5ef39e3fd5f45ac67db6d5
Syifaul M <syifa.ulm01@gmail.com>
2023-10-07 21:57:51 +0700

    edit: add button logic

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -67,47 +67,56 @@ class DashboardPage extends Component<any, State>{
         
     }
 
+
     render(){
-       const { dataProps } = this.props;
-       const data = dataProps.data;
+        const { dataProps } = this.props;
+        const data = dataProps.data;
+
+        const { userProps } = this.props;
+        const role = userProps.data.user.role;
+        const isAdmin = role === 'Admin';
 
         return (
             <div className="text-gray-600 font-body">
                 <Navbar />
-                <div className="flex p-3">
-                    <Link
-                        to={"/product-page"}
-                        className="m-3 lg:w-[15%] text-sm flex items-center w-[50%] px-5 py-2 font-bold text-white rounded-lg mb-8 lg:px-5 lg:py-2 md:text-base bg-blue-400 hover:opacity-80 hover:shadow-lg transition duration-500"
-                    >
-                        <span className="fill-current mr-3">
-                        <svg
-                            role="img"
-                            width="20"
-                            viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg"
+                {isAdmin ? (
+                    <div className="flex p-3">
+                        <Link
+                            to={"/product-page"}
+                            className="m-3 lg:w-[15%] text-sm flex items-center w-[50%] px-5 py-2 font-bold text-white rounded-lg mb-8 lg:px-5 lg:py-2 md:text-base bg-blue-400 hover:opacity-80 hover:shadow-lg transition duration-500"
                         >
-                            <path d="M18 13.496h-4.501v4.484h-3v-4.484H6v-2.99h4.5V6.021h3.001v4.485H18v2.99zM21 .041H3C1.348.043.008 1.379 0 3.031v17.94c.008 1.65 1.348 2.986 3 2.988h18c1.651-.002 2.991-1.338 3-2.988V3.031c-.009-1.652-1.348-2.987-3-2.99z" />
-                        </svg>
-                        </span>
-                        Tambah Product
-                    </Link>
-                    <Link
-                        to={"/category"}
-                        className="m-3 lg:w-[15%] text-sm flex items-center w-[50%] px-5 py-2 font-bold text-white rounded-lg mb-8 lg:px-5 lg:py-2 md:text-base bg-blue-400 hover:opacity-80 hover:shadow-lg transition duration-500"
-                    >
-                        <span className="fill-current mr-3">
-                        <svg
-                            role="img"
-                            width="20"
-                            viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg"
+                            <span className="fill-current mr-3">
+                                <svg
+                                    role="img"
+                                    width="20"
+                                    viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path d="M18 13.496h-4.501v4.484h-3v-4.484H6v-2.99h4.5V6.021h3.001v4.485H18v2.99zM21 .041H3C1.348.043.008 1.379 0 3.031v17.94c.008 1.65 1.348 2.986 3 2.988h18c1.651-.002 2.991-1.338 3-2.988V3.031c-.009-1.652-1.348-2.987-3-2.99z" />
+                                </svg>
+                            </span>
+                            Tambah Product
+                        </Link>
+                        <Link
+                            to={"/category"}
+                            className="m-3 lg:w-[15%] text-sm flex items-center w-[50%] px-5 py-2 font-bold text-white rounded-lg mb-8 lg:px-5 lg:py-2 md:text-base bg-blue-400 hover:opacity-80 hover:shadow-lg transition duration-500"
                         >
-                            <path d="M18 13.496h-4.501v4.484h-3v-4.484H6v-2.99h4.5V6.021h3.001v4.485H18v2.99zM21 .041H3C1.348.043.008 1.379 0 3.031v17.94c.008 1.65 1.348 2.986 3 2.988h18c1.651-.002 2.991-1.338 3-2.988V3.031c-.009-1.652-1.348-2.987-3-2.99z" />
-                        </svg>
-                        </span>
-                        Tambah Category
-                    </Link>
-                </div>
+                            <span className="fill-current mr-3">
+                                <svg
+                                    role="img"
+                                    width="20"
+                                    viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path d="M18 13.496h-4.501v4.484h-3v-4.484H6v-2.99h4.5V6.021h3.001v4.485H18v2.99zM21 .041H3C1.348.043.008 1.379 0 3.031v17.94c.008 1.65 1.348 2.986 3 2.988h18c1.651-.002 2.991-1.338 3-2.988V3.031c-.009-1.652-1.348-2.987-3-2.99z" />
+                                </svg>
+                            </span>
+                            Tambah Category
+                        </Link>
+                    </div>
+                    ) : (
+                        <div className="mb-16"></div>
+                    )}
                 <div className=" w-11/12 m-auto">
                     <div className="grid lg:grid-cols-5">
                         {data?.map((product:any) => {
@@ -123,6 +132,7 @@ class DashboardPage extends Component<any, State>{
                                                 <p className="text-gray-700 text-base">{product.price} : {product.stock} pcs</p>
                                             </div>
                                         </Link>
+                                        {isAdmin ? (
                                         <div className="flex">
                                             <button
                                                 className="mr-2 border-2 rounded-lg bg-white hover border-gray-500 hover:bg-gray-500 text-gray-500 hover:text-white flex items-center justify-center w-1/4 h-9"
@@ -133,8 +143,12 @@ class DashboardPage extends Component<any, State>{
                                                 className="border-2 rounded-lg bg-white hover border-red-500 hover:bg-red-500 text-red-500 hover:text-white flex items-center justify-center w-1/3 h-9 p-2"
                                                 onClick={() => this.deleteProductById(product._id)}>delete
                                             </button>
-                                            
                                         </div>
+                                        ):(
+                                            <div className="mb-5">
+                                                
+                                            </div>
+                                        )}
                                     </div>
                                 </div>
                             )
