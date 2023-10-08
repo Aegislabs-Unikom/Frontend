32c04fb0d3ea6d6929ec9eff73830a2ed5ad89b9
Syifaul M <syifa.ulm01@gmail.com>
2023-10-08 22:18:39 +0700

    edit: update cart logo

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { withRouter } from '../helper/withRouter';
 import { logoutAsync } from '../store/auth/AuthSlice';
+import { getAllCart } from '../store/cart/CartSlice';
 
 type NavbarState = {
   open: boolean;
@@ -15,6 +16,13 @@ class Navbar extends Component<any, NavbarState> {
     this.state = {
       open: false,
     };
+  }
+
+  componentDidCatch() {
+    this.props.getAllCart()
+        .then(function name(params: any) {
+        //   console.log(params);       
+        });
   }
 
   handleButtonClick = () => {
@@ -40,6 +48,9 @@ class Navbar extends Component<any, NavbarState> {
   render() {
     const { open } = this.state;
     const userProps = this.props.dataUser;
+
+    const { cartProps } = this.props;
+    const data = cartProps.data.length;
 
     return (
       <nav className="bg-gray-700 p-3">
@@ -81,7 +92,9 @@ class Navbar extends Component<any, NavbarState> {
                   <div className="flex justify-center items-center mr-5 ml-3">
                     <div className="relative">
                       <div className="t-0 absolute left-4">
-                        <p className="flex h-2 w-2 items-center justify-center rounded-full bg-red-500 p-3 text-xs text-white">3</p>
+                        <p className="flex h-2 w-2 items-center justify-center rounded-full bg-red-500 p-3 text-xs text-white">
+                          {data}
+                        </p>
                       </div>
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" className="file: mt-2 h-6 w-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
@@ -97,7 +110,8 @@ class Navbar extends Component<any, NavbarState> {
 }
 
 const mapStateToProps = (state: any) => ({
-    dataUser: state.auth
+    dataUser: state.auth,
+    cartProps: state.cart
 });
 
 const mapDispatchToProps = {
