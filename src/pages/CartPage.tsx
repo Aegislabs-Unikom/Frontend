074b5b7a80d06108bf7a6f1991843891539bdd22
Syifaul M <syifa.ulm01@gmail.com>
2023-10-17 16:57:10 +0700

    edit cartpage before conflict

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -3,9 +3,15 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import React, { Component } from "react";
 import { getAllProducts, deleteProduct } from "../store/product/ProductSlice";
-import { addToCart, getAllCart, deleteProductInCart } from "../store/cart/CartSlice";
+import { addToCart, getAllCart } from "../store/cart/CartSlice";
 import { statusPaymentOrder, processCartToPayment } from "../store/paymentOrder/PaymentSlice";
 import { withRouter } from "../helper/withRouter";
+
+declare global {
+    interface Window {
+      snap: any; // Change 'any' to the actual type if you have it
+    }
+  }
 
 type Products = {
     id: string;
@@ -17,7 +23,9 @@ type Products = {
   
   type State = {
     productsData: Products[];
-    quantity: number; //test
+    quantity: number; //test;
+    loading: boolean;
+    token:string;
   };
 
 class CartPage extends Component<any, State>{
@@ -26,7 +34,9 @@ class CartPage extends Component<any, State>{
     
         this.state = {
             productsData: [],
-            quantity: 0 //test
+            quantity: 0, //test
+            loading: true,
+            token:"",
         };
     }
     
@@ -40,8 +50,7 @@ class CartPage extends Component<any, State>{
             onSuccess: async (result:any) => {
               this.setState({ token: "" });
               await this.statusPayment("Success");
-              //this.props.router.navigate(`/`);
-              window.location.href = "/";
+              this.props.router.navigate(`/`);
             },
             onPending: async (result:any) => {
               this.setState({ token: "" });
@@ -74,21 +83,26 @@ class CartPage extends Component<any, State>{
           };
     }
 
+    // componentDidUpdate(prevProps: any) {
+    //     console.log(this.props.dataProps.data.length);
+    //     console.log(prevProps.dataProps.data.length);
+    //     // if (this.props.dataProps.data.length !== prevProps.dataProps.data.length) {
+    //     //     this.getData();
+    //     // }
+    //     if (prevProps.dataProps.data.length !== this.props.dataProps.data.length) {
+    //         console.log('pokemons state has changed.');
+    //         this.getData();
+    //       }
+    // }
     count = 0;
-    getData = async () => {
-        this.props.getAllCart()
-        .then(() => {
-          // this.dataProps = this.props;
-          const { cartProps } = this.props;
-          // this.dataProduct = this.props.dataProps.data;
-          // console.log(dataProps);
-          // const data = dataProduct.data;
 
-          this.setState({ 
-              productsData: cartProps.data
-           });
-        });
+    async getData () {
+        this.props.getAllCart();
+        // .then(function name(params: any) {
+        //   console.log(params);       
+        // });
     }
+
     editProductById = async (productId: string) => {
         this.props.router.navigate(`/product-page/${productId}`);
     }
@@ -105,21 +119,6 @@ class CartPage extends Component<any, State>{
                     console.error("Error deleting product:", error);
                 });    
         }
-        
-    }
-
-    deleteProductInCart = async (productId: string) => {
-        const confirm = window.confirm('Delete this product?');
-        if (confirm) {
-            console.log(productId);
-            await this.props.deleteProductInCart({ id: productId })
-                .then(()=>{
-                    this.getData();
-                })
-                .catch((error: any) => {
-                    console.error("Error deleting product:", error);
-                });    
-        }
     }
 
     async processPayment() {
@@ -129,6 +128,19 @@ class CartPage extends Component<any, State>{
           this.setState({ token }, () => {
             console.log("Token state:", this.state.token); // Log the updated value.
           });
+        // // const { paymentProps } = this.props;
+        // await this.props.processCartToPayment()
+        // .then((params: any) =>{
+        //   console.log("ini params");
+        //   console.log(params.payload.token);
+        //   console.log("ini state");
+        //   this.setState({token: params.payload.token});
+        //   console.log(this.state.token);
+        // });
+        // console.log(this.setState({ token: paymentProps.token }));
+        // this.setState({ token: paymentProps.token }, () => {
+        //     console.log(this.state.token); // This will log the updated value.
+        // });
     } catch (error) {
         console.error("Error processing payment:", error);
     }
@@ -136,19 +148,76 @@ class CartPage extends Component<any, State>{
 
     statusPayment = async (newStatus:string) => {
         try {
-          this.props.statusPaymentOrder({ status: newStatus });
+          this.props.statusPaymentOrder(newStatus);
         } catch (error) {
           console.error("Error processing payment:", error);
         }
     }    
 
+    checkOut = async (id: string) => { //mau diganti
+        if (this.state.quantity === 0) {
+          alert("barang kosong");
+          return;
+        } 
+        try {
+          const { quantity } = this.state;
+          await this.props
+            .addToCart({ id, quantity })
+            .then(
+                console.log("berhasil")
+            )
+            .catch((error: any) => {
+              console.error(error);
+            });
+        } catch (error) {
+          console.error(error);
+        }
+    };
+
+    totalAmount(data: any){
+
+    }
+
+    onclick(type: any, quantity: number){ //test
+        console.log(quantity);
+        if (quantity === 0 && type === 'add') {
+            quantity =+ 1;
+          } else if (quantity > 0 ){
+            console.log("tambah");
+            (type === 'add' ? quantity =+ 1 : quantity =- 1 )
+          }
+        // this.setState(prevProps => {
+        //   if (prevProps.quantity === 0 && type === 'add') {
+        //     return {quantity: prevProps.quantity + 1};
+        //   } else if (prevProps.quantity > 0 ){
+        //     console.log("tambah");
+        //     console.log(prevProps.quantity);
+        //     return {quantity: type === 'add' ? prevProps.quantity + 1: prevProps.quantity - 1}
+        //   }
+        //   return null;
+        // });
+      }
+
+    handleCheckoutClick = (id: string) => { //test
+        this.checkOut(id);
+    };
+
+
     render(){
         const { cartProps } = this.props;
         const data = cartProps.data;
+        // console.log(data);
+        // const amount = data.
+        // const grandTotal = 0;
+        const cartIsNull = cartProps.data.length === 0;
 
         const { userProps } = this.props;
         const role = userProps.data.user.role;
         const isAdmin = role === 'Admin';
+
+        // if (this.state.loading) {
+        //     return <p>Loading...</p>;
+        // }
 
         return (
             <div className="text-gray-600 font-body">
@@ -191,60 +260,13 @@ class CartPage extends Component<any, State>{
                     ) : (
                         <div className="mb-16"></div>
                     )}
-<<<<<<< Updated upstream
-                <div className=" w-11/12 m-auto">
-                    <div className="grid lg:grid-cols-5">
-                        {data?.map((cart:any) => {
-                            return ( //buat komponen terpisah
-                            <div key={cart.product._id} className="max-w-sm w-full lg:max-w-full mb-4">
-                                    <div className="m-auto w-60 h-96 border-r border-b border-l border-gray-400 lg:border-l lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-                                        <Link
-                                            to={`/product/${cart.product._id}`}>
-                                            <div className="mb-3">
-                                                <img src={cart.product.image} alt="" className="max-h-56 m-auto"/>
-                                                <p className="text-gray-700 text-base mt-2">{cart.product.nama_produk}</p>
-                                                <p className="text-gray-700 text-base mt-1">{cart.product.description}</p>
-                                                <p className="text-gray-700 text-base">{cart.product.price} pcs</p>
-                                                <p className="text-gray-700 text-base">{cart.quantity}</p>
-                                            </div>
-                                        </Link>
-                                        {isAdmin ? (
-                                        <div className="flex">
-                                            <button
-                                                className="mr-2 border-2 rounded-lg bg-white hover border-gray-500 hover:bg-gray-500 text-gray-500 hover:text-white flex items-center justify-center w-1/4 h-9"
-                                                onClick={() => this.editProductById(cart.product._id)}
-                                                > edit
-                                            </button>
-                                            <button
-                                                className="border-2 rounded-lg bg-white hover border-red-500 hover:bg-red-500 text-red-500 hover:text-white flex items-center justify-center w-1/3 h-9 p-2"
-                                                onClick={() => this.deleteProductById(cart.product._id)}>delete
-                                            </button>
-                                        </div>
-                                        ):(
-                                            <div className="mb-5">
-                                                <div className="flex border-gray-200">
-                                                    {/* <div className="flex"> //tambah dan kurang
-                                                        <button onClick={this.onclick.bind(this, 'add',cart.quantity )} className="mr-2">+</button>
-                                                            <input type="text" disabled value={this.count = cart.quantity} onChange={(e) => this.setState({ quantity: parseInt(e.target.value) })} className="w-6 text-center"/>
-                                                        <button onClick={this.onclick.bind(this, 'sub', cart.quantity)} className="ml-2">-</button>
-                                                    </div> */}
-                                                    <button className="flex ml-auto text-white bg-red-500 border-0 py-2 px-2 focus:outline-none hover:bg-red-600 rounded" onClick={() => this.handleCheckoutClick(cart._id)}>Checkout</button>
-                                                </div>
-                                            </div>
-                                        )}
-                                    </div>
-                                </div>
-                            )
-                        })}
-                    </div>
-=======
                 <div className="max-w-2xl mx-auto p-4">
                     <h1 className="text-2xl font-semibold mb-4">Your Cart</h1>
                     {cartIsNull? (
                         <h1 className="text-2xl font-semibold mb-4">Cart is Empty...</h1>
                     ):(
                         <ul>
-                        {this.state.productsData?.map((item:any) => (
+                        {data.map((item:any) => (
                             <li
                             key={item.product._id}
                             className="border border-gray-300 p-4 mb-4 flex items-center"
@@ -256,7 +278,7 @@ class CartPage extends Component<any, State>{
                                 className="w-16 h-16 object-cover"
                                 />
                             </div>
-                            <div className="ml-4 w-9/12">
+                            <div className="ml-4">
                                 <h2 className="text-lg font-semibold">
                                 {item.product.nama_produk}
                                 </h2>
@@ -265,10 +287,6 @@ class CartPage extends Component<any, State>{
                                 <p className="text-gray-600">Total: {(item.quantity)*(item.product.price)}</p>
                                 this.grandTotal =+ {(item.quantity)*(item.product.price)}
                             </div>
-                            <button
-                              className="border-2 rounded-lg bg-white hover border-red-500 hover:bg-red-500 text-red-500 hover:text-white flex items-center justify-center w-1/8 h-9 p-2 float-right"
-                              onClick={() => this.deleteProductInCart(item.product._id)}>delete
-                            </button>                
                             </li>
                         ))}
                             <button
@@ -279,9 +297,7 @@ class CartPage extends Component<any, State>{
                             </button>
                         </ul>
                     )}
->>>>>>> Stashed changes
                 </div>
-                
             </div>
         )
     }
@@ -290,21 +306,17 @@ class CartPage extends Component<any, State>{
 const mapStateToProps = (state: any) => ({
     userProps: state.auth,
     dataProps: state.products,
-    cartProps: state.cart
+    cartProps: state.cart,
+    paymentProps: state.payment,
   });
   
   const mapDispatchToProps = {
     getAllProducts, 
     deleteProduct,
     addToCart,
-<<<<<<< Updated upstream
-    getAllCart
-=======
     getAllCart,
     statusPaymentOrder,
-    processCartToPayment,
-    deleteProductInCart
->>>>>>> Stashed changes
+    processCartToPayment
   };
 
 export default connect(mapStateToProps,mapDispatchToProps)(withRouter(CartPage));
