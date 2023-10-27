650ff54cfcf8eb7cd02d1c4b305f9badd5d80b8c
Syifaul M <syifa.ulm01@gmail.com>
2023-10-27 09:37:42 +0700

    edit: edit product info

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -162,7 +162,7 @@ class DashboardPage extends Component<any, State>{
                                                 <img src={product.image} alt="" className="max-h-56 m-auto"/>
                                                 <p className="text-gray-700 text-base mt-2">{product.nama_produk}</p>
                                                 <p className="text-gray-700 text-base mt-1">{product.description}</p>
-                                                <p className="text-gray-700 text-base">Rp. {product.price} : {product.stock} pcs</p>
+                                                <p className="text-gray-700 text-base">Rp. {product.price}</p>
                                                 <p className="text-gray-700 text-base">{product.stock} pcs</p>
                                             </div>
                                         </Link>
